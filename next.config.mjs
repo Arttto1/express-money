043b88369bc703dc2950ec8948818baab15945aa
Arttto1 <arthurnunes10@live.com>
2024-12-09 02:10:53 -0300

    09/12

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/svum',
-        permanent: true,
-      },
-    ]
-  },
-};
+const nextConfig = {};
+
 export default nextConfig;
